b5caab07116c31460d3f6577d11bb97099b37ac1
Berenice <bunchy0303@gmail.com>
2026-01-07 01:13:26 -0300

    Parallax added

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,22 +1,35 @@
 extends CharacterBody2D
 
-@export var SPEED = 400.0
-@export var JUMP_VELOCITY = -500.0 # negativo significa arriba
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0 # negativo significa arriba
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity_jump = 1200.0 #gravedad interna para el salto fingido
+var z_velocity = 0.0 #velocidad vertical del salto
+var z_pos = 0.0 #altura actual con respecto al suelo
+
+@onready var sprite = $Sprite2D # referencia de la imagen
 
 func _physics_process(delta: float) -> void:
-	#Jump logic
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	#Hall logic
-	var input_dir_y := Input.get_axis("ui_up", "ui_down")
 	
-	if input_dir_y:
-		velocity.y = input_dir_y * (SPEED/2)
+	#Hall logic
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity.y = input_dir.y * SPEED
+	velocity.x = 0
+	
+	#Fake jump logic
+	if Input.is_action_just_pressed("jump") and z_pos == 0:
+		z_velocity = JUMP_VELOCITY
+		
+	if z_pos < 0 or z_velocity < 0:
+		z_velocity += gravity_jump * delta
+		z_pos += z_velocity * delta
+	
+	if z_pos > 0:
+		z_pos = 0
+		z_velocity = 0
+		
+	#Apply to sprite
+	sprite.position.y = z_pos
 		
 	move_and_slide()
